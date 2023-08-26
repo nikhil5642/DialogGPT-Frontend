be58ae0e79be38ab126edc7c2cb91fef9c56ff53
Nikhil agrawal <nikhilagrawal5642@gmail.com>
2023-08-27 00:48:48 +0530

    temp stopped reload

--- pages/chatbot/[chatbotID].js
+++ pages/chatbot/[chatbotID].js
@@ -2,9 +2,22 @@ import { useRouter } from "next/router";
 import Head from "next/head";
 import styles from "../styles/defaults.module.scss";
 import ChatBotEditor from "../../src/components/chatbot-editor/chatbot-editor";
+import React, { useEffect } from "react";
 
-export default function GamePlayScreen() {
+export default function ChatbotPage() {
 	const { chatbotID } = useRouter().query;
+	useEffect(() => {
+		const handleBeforeUnload = (e) => {
+			e.preventDefault();
+			e.returnValue = "Are you sure you want to leave or refresh this page?";
+		};
+
+		window.addEventListener("beforeunload", handleBeforeUnload);
+
+		return () => {
+			window.removeEventListener("beforeunload", handleBeforeUnload);
+		};
+	}, []);
 
 	return (
 		<>
@@ -12,7 +25,7 @@ export default function GamePlayScreen() {
 				<Head>
 					<title>Home</title>
 					<meta name="My Chatbots" content="List of already trained chatbots" />
-					<link rel="canonical" href="https://chessmeito.com/my-chatbots" />
+					<link rel="canonical" href="https://diaglogGPT.com/my-chatbots" />
 				</Head>
 				<div className={styles.container}>
 					<ChatBotEditor botID={chatbotID} />
