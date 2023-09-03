75e617024103ee2a6dd6e3f7bc2f2fbbf81244b9
Nikhil agrawal <nikhilagrawal5642@gmail.com>
2023-09-04 03:39:10 +0530

    Stripe live env key set

--- src/helper/private-keys.js
+++ src/helper/private-keys.js
@@ -1,4 +1,4 @@
 export const PrivateKeys = {
 	NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-		"pk_test_51NlWuWSBubjVCHLvJvHJClZcCKQK2kpzOeVxfxo8ezGhSUVBhulFL0mFaZyLUkzVfwKjqxCOndm4B5Dgo9Batpr700tXFkYm9u",
+		"pk_live_51NlWuWSBubjVCHLvmAuxOEtkGLiIfo6elgmbwpinzRLOQyp56idmwOwMaWC0s9y2NMr5VsaesVn8mAHEkZk6iabx00s7edFOXH",
 };
