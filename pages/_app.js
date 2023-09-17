f2a19b162da51f09d2c55e63f87d72df077860d9
Nikhil agrawal <nikhilagrawal5642@gmail.com>
2023-09-17 11:15:03 +0530

    clarity introduced

--- pages/_app.js
+++ pages/_app.js
@@ -40,6 +40,19 @@ function MyApp({ Component, pageProps }) {
 						gtag('config', 'AW-880583637');
 					`}
 				</script>
+				{!isIsolated && (
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+						(function(c,l,a,r,i,t,y){
+							c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+							t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+							y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+						})(window, document, "clarity", "script", "iwf4xtf2rv");
+						`,
+						}}
+					/>
+				)}
 			</Head>
 			<IsolationContext.Provider value={isIsolated}>
 				{isIsolated ? (
