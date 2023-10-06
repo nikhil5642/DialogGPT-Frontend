0eb0094681f293ff0651077dacd87f80c7b1c4b7
Nikhil agrawal <nikhilagrawal5642@gmail.com>
2023-10-06 18:46:14 +0530

    styles fixed

--- src/components/chatbot-settings/chatbot-settings.js
+++ src/components/chatbot-settings/chatbot-settings.js
@@ -3,8 +3,7 @@ import styles from "./chatbot-settings.module.scss";
 import GeneralSettings from "./general-settings/general-settings";
 import ModelSettings from "./model-settings/model-settings";
 import ChatInterfaceSettings from "./chat-interface-settings/chat-interface-settings";
-import LoadingButton from "../loading-button/loading-button";
-import DanzerSettings from "./danzer-settings/danzer-settings";
+import DangerSettings from "./danger-settings/danger-settings";
 export default function ChatBotSettings({ data, setData }) {
 	return (
 		<div className={styles.settingsContainer}>
@@ -17,7 +16,7 @@ export default function ChatBotSettings({ data, setData }) {
 			<ChatInterfaceSettings botID={data.id} />
 			<br></br>
 			<br></br>
-			<DanzerSettings botID={data.id} />
+			<DangerSettings botID={data.id} />
 		</div>
 	);
 }
